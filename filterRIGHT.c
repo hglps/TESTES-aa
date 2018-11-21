7d2ca6217be7e4b5038ce81cfa024ca2c61f7a1b
hglps <44714747+hglps@users.noreply.github.com>
2018-11-21 15:56:15 -0300

    Create filterRIGHT.c

---
+++ filterRIGHT.c
@@ -0,0 +1,40 @@
+#include <stdio.h> /// TO BE CONTINUED
+#include <stdlib.h>
+
+int even(int ar)
+{
+  return !(ar%2);
+}
+
+int number((int)(*f)(int*), int *array)
+{
+  int z;
+    for(z=0;)
+}
+
+void main()
+{
+  int *a=malloc(sizeof(int));
+  int i=0, total;
+  char *word = malloc(sizeof(char)*5);
+
+  while (scanf("%d", *(a+i)!= EOF)
+  {
+    realloc(int *a, i);
+    i++;
+  }
+ total = i;
+  gets(word);
+  if(strcmp(word, "par") == 0)
+  {
+    number(par, a);
+  }
+  else if(strcmp(word, "impar") == 0)
+  {
+    number(impar, a);
+  }
+  // for(i = 0; i < total; i++ )
+  // {
+  //   printf("%d ", *(a+i));
+  // }
+}
