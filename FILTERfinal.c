c44e7967c032b892c3fba2f1999fc238df435af3
hglps <44714747+hglps@users.noreply.github.com>
2018-11-21 17:19:31 -0300

    Update FILTERfinal.c

--- FILTERfinal.c
+++ FILTERfinal.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int odd(int ar2)/// IMPAR
+int odd(int ar2)/// analyze the number, if it's even..
 {
-  printf("AAAAAAAAAAA");
   return (ar2%2);
 }
 
-int even(int ar) /// PAR
+int even(int ar) /// analyze the number, if it's even..
 {
-  printf("BBBBBBBBBB");
   return !(ar%2);
 }
 
-void filterr(int *array, int size, int (*f)(int))
+void filterr(int *array, int size, int (*f)(int))// filter the array
 {
   int z;
   for(z=0; z< size;z++)
   {
-    if(f(array[z]));
+    if(f(*(array+z))) // if condition is true;
     {
-      printf("%d ", array[z]);
+      printf("%d ", *(array+z));// print the number and so on
     }
   }
 
@@ -28,30 +26,31 @@ void filterr(int *array, int size, int (*f)(int))
 
 void main()
 {
-  int *a=malloc(sizeof(int));
+  int *a=malloc(sizeof(int)); // allocate 1 int to start
   int i=0, total;
-  char *word = malloc(sizeof(char)*5);
+  char *word = malloc(sizeof(char)*5); // allocate 1 char just to verify later :)
 
-  while (scanf("%d", (a+i)))
+  while (scanf("%d", (a+i)))// scan the array until it ends
   {
-    if(*(a+i)==-1) break;
-    a = (int*) realloc(a, sizeof(double)*(i+1));
-    i++;
+    if(*(a+i)==-1) break;// if I press -1, finish it
+    a = (int*) realloc(a, sizeof(double)*(i+1));// allocate 1 int by 1
+    i++;// adds ""to the"" array
   }
-  total = i;
-  ///filterr(a,total, even); /// CHAMADA NORMAL
-  // //getchar();
-  // //gets(word);
-  //
-  // if(strcmp(word, "par") == 0) /// if even..
-  // {
-  //   printf("paaaaaaaar\n");
-  //   filterr(a,total,even); // use even funcion
-  // }
-  // else if(strcmp(word, "impar") == 0) // if odd..
-  // {
-  //   printf("impaaaar\n");
-  //   filterr(a,total,odd);// use odd func.
-  // }
 
+  total = i;// gets the size of the array
+  getchar();// gets the "enter" after scanning the string
+  gets(word);// scan the string
+
+  if(strcmp(word, "par") == 0) /// if string == "par"..
+  {
+    filterr(a,total,even); // use even funcion
+  }
+  else if(strcmp(word, "impar") == 0) // if string == "impar"..
+  {
+    filterr(a,total,odd);// use odd func.
+  }
+
+  free(a);
+  free(word);
+/////THE CODE IS OVER :)
 }
