fc9560d62c1c6184ad4d4adb4e14e8b2664e3d73
hglps <44714747+hglps@users.noreply.github.com>
2019-01-23 14:40:10 -0300

    Update testLINKED_LIST.c

--- testLINKED_LIST.c
+++ testLINKED_LIST.c
@@ -1,30 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-/// to be finished..
-struct node
+
+typedef struct node
 {
-  void *item;/// generic item; pointer to any type
-  node *next;
-};
-/// searching with Higher Order function;
-/// must declare all the possible functions;
-/// while it isn't on the last position...
-/// keep changing the HEAD to the next one and analyzing each item;
-/// if the item is found, return the actual "position" of the item;****to_be_finished****
-node* search(node *head, void *item, int (*equal)(void *item1, void *item2))
+	void *item;
+	node *next;
+}node;
+
+int if_empty(node *head)/// if the list is empty, return the NULL
 {
-  while (head != NULL)
-  {
-    if((*equal) (head->item, item)) return head;
-    head = head->next;
-  }
-  return NULL;
+	return (head == NULL);
 }
-/// begin explanation later..****to_be_finished****
-node* add_at_begg(node *head, void *item)
+
+node* createLIST() // what it said...
 {
-  node *new_node = (node*) malloc(sizeof(node));
-  new_node->item = item;
-  new_node->next = head;
-  return new_node;
+	return NULL;
+}
+
+node* search(node *head, int item)
+{
+	while(head != NULL)
+	{
+		if(head->item) return head;
+		head = head->next;
+	}
+	return NULL;
+}
+
+node* add_at_begg(node *head, int *item)
+{
+	node *new_node = (node*) malloc(sizeof(node));
+	new_node->item = item;/// check void
+	new_node->next = head;/// check void
+	return new_node;
+}
+void print_list(node *list)
+{
+	while(list !=NULL)
+	{
+		printf("%d\n", list->item);
+		list = list->next;
+	}
+}
+
+void main()/// read numbers and print the linked list
+{
+	node* list = createLIST();
+	int item;
+	int n;
+	scanf("%d", &n);
+	while(n--)
+	{
+		scanf("%d", &item);
+		list = add_at_begg(list, item);
+	}
+	
 }
