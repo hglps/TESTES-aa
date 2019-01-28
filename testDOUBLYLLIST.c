3a076048232a6709795e74c32cdae7a258c7a44c
hglps <44714747+hglps@users.noreply.github.com>
2019-01-28 16:27:04 -0300

    Update testDOUBLYLLIST.c

--- testDOUBLYLLIST.c
+++ testDOUBLYLLIST.c
@@ -72,6 +72,74 @@ void main()
 }
 
 
+/////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+///// DOUBLY LINKED LIST
+typedef struct node node;
+struct node
+{
+	int item;
+	node *next;
+	node *previous;
+};
+
+node* create_list()
+{
+	node* blabla= (node*) malloc(sizeof(node));
+	blabla->next = NULL; blabla->previous= NULL;
+	blabla->item = NULL;
+	return (blabla);
+}
+
+node* add_general(node *head, int item)
+{
+	node* new_node = (node*)malloc(sizeof(node));
+	new_node->item = item;
+	new_node->next = NULL;///
+	new_node->previous = head;
+	head->next = new_node;
+	
+	if(head!=NULL) head=new_node;///
+	return new_node;
+}
+
+void print_list(node *list)
+{ 
+	while(1)
+	{
+	    printf("%d ",list->item);
+		list = list->previous;
+		
+	    if (list->previous==NULL) break;
+		
+	}
+}
+
+void main()
+{
+	node* list = create_list();
+	int n,i;
+	node* tail;
+	//scanf("%d", &n);
+	while(scanf("%d", &n) != EOF)
+	{
+		list = add_general(list,n);
+		tail=list;
+	}
+
+	print_list(tail);
+
+	free(list);
+}
+
+
+
+
+
+
+
 
 
 
