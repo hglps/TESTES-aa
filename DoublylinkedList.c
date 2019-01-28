b7cb6588eeb0034dabb9dcc9129734a0866f20bf
hglps <44714747+hglps@users.noreply.github.com>
2019-01-28 15:34:24 -0300

    Update DoublylinkedList.c

--- DoublylinkedList.c
+++ DoublylinkedList.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+///// DOUBLY LINKED LIST
 typedef struct node node;
-
 struct node
 {
 	int item;
-	node *previous;
 	node *next;
+	node *previous;
 };
 
-node* createLIST()
+node* create_list()
 {
-	return NULL;
+	node* blabla= (node*) malloc(sizeof(node));
+	blabla->next = NULL; blabla->previous= NULL;
+	blabla->item = NULL;
+	return (blabla);
 }
 
-node* add_at_begg(node *head, int item)
+node* add_general(node *head, int item)
 {
-	node *new_node = (node*) malloc(sizeof(node));
+	node* new_node = (node*)malloc(sizeof(node));
 	new_node->item = item;
-	new_node->next = head;
-	new_node->previous = NULL; /// *** analyze;
-	if(head!= NULL) head->previous = new_node;
-
+	new_node->next = NULL;///
+	new_node->previous = head;
+	head->next = new_node;
+	
+	if(head!=NULL) head=new_node;///
 	return new_node;
 }
 
+void print_list(node *list)
+{ 
+	while(list->item != NULL)
+	{
+		printf("%d\n", list->item);
+		list = list->previous;
+	}
+}
 
-/// ......
+void main()
+{
+	node* list = create_list();
+	int n,i;
+	//scanf("%d", &n);
+	for(i=0;i<5;i++)
+	{
+		scanf("%d", &n);
+		list = add_general(list,n);
+	}
+	printf("\n");
+
+	print_list(list);
+
+	free(list);
+}
