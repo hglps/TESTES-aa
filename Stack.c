7f5f2fdb40d340043378fba6176764183c416cdd
hglps <44714747+hglps@users.noreply.github.com>
2019-02-10 14:09:16 -0300

    Create Stack.c

---
+++ Stack.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/// STACK USING LINKED LIST
+#define MAX_SIZE 10;
+typedef struct node node;
+typedef struct stack stack;
+struct node
+{
+	int item;
+	node *next;
+} 
+struct stack
+{
+	node *top;
+}
+
+stack* create_stack()
+{
+	stack *new_stack = (stack*) malloc(sizeof(stack));
+	new_stack->top = NULL;
+	return new_stack;		
+}
+
+int empty(stack *st)
+{
+	return (stack->top == NULL);
+} 
+
+void push(stack *stack, int item)
+{
+	node *new_node = malloc(sizeof(node));
+	new_node->item = item;
+	new_node->next = stack->top;
+	stack->top = new_node;
+}
+
+int pop(stack *st)
+{
+	if(empty(st)) {  printf("STACK UNDERFLOW\n"); return -1; }	
+	int it= node->item;/// save it, then delete it
+	st->top = node->next;
+	node->next = NULL;
+	return it;	
+}
+
+int peek(stack *st)
+{
+	if(empty(st)) { printf("STACK UNDERFLOW\n")/ return -1;}
+	
+	return node->item;
+}
+
+void main()
+{
+	stack *stack = create_stack;
+	///stuff
+	///stuff
+	free(stack);
+}
