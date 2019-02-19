d44f9cd9ab151b5711baf6f8ebe6a15da4faae15
hglps <44714747+hglps@users.noreply.github.com>
2019-02-19 13:02:13 -0300

    Update create_btree_()().c

--- create_btree_()().c
+++ create_btree_()().c
@@ -1,1 +1,27 @@
-olhar fotos :3
+btree* form_btree(char *text)
+{
+  btree *bt = create_empty();
+  int integer, i=0,index=0;
+  char extractText[100];
+
+  while(text[index] == '(') index+=1;
+  if(text[index] == ')')
+  {
+    index++;
+    return NULL;
+  }
+  else
+  {
+    while(text[index] != '(')
+    {
+      extractText[i++] = text[index];
+      index+=1;
+    }
+    extractText[i] = '\0';
+    integer = atoi(extractText);
+    bt = create_btree(integer,NULL,NULL);
+    bt->left = form_btree(text);
+  }
+  index+=1;
+  return bt;
+}
